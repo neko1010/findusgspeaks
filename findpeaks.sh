fac428763e8884c71a6010af8cafb420a7ccc005
Nick Kolarik <nicholaskolarik@gmail.com>
2017-12-18 12:09:12 -0500

    added a header

--- findpeaks.sh
+++ findpeaks.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+#Name:
+#  findpeaks.sh: finds largest peak values in USGS water time series files
+#
+#Usage: findpeaks.sh FILES
+#
+#Description:
+#	Longer description
+
+# grab each line beginning with 'USGS', only columns 3-4, sort numerically,
+#in reverse, by the second column, and return the first line of the head 
 for file in $@
 do
     echo $file
     grep ^USGS $file | cut -f 3-4 | sort -n -r -k2 | head -1
     echo 
 done
-# grab each line beginning with 'USGS', only columns 3-4, sort numerically, in reverse, by the second column, and return the first line of the head# :
 

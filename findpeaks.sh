044bf0d9a818917d55c9c4de6ca8fbfaaafaaced
Nick Kolarik <nicholaskolarik@gmail.com>
2017-12-18 12:54:12 -0500

    removed dummy files

--- findpeaks.sh
+++ findpeaks.sh
@@ -9,12 +9,10 @@
 
 # grab each line beginning with 'USGS', only columns 3-4, sort numerically,
 #in reverse, by the second column, and return the first line of the head 
-#dummy line
 for file in $@
 do
     echo $file
     grep ^USGS $file | cut -f 3-4 | sort -n -r -k2 | head -1
     echo 
 done
-#new line
 
